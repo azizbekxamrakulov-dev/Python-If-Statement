85bb2c1fa1888d91dd3f9e4d1ea7f67adc8d88c2
azizbekxamrakulov-dev <azizbek.xamrakulov@gmail.com>
2025-12-10 23:31:33 +0500

    Update task11.py

---
+++ task11.py
@@ -0,0 +1,10 @@
+oy = int(input("Oy raqami: ")) 
+ 
+if oy in [12, 1, 2]: 
+    print("Qish") 
+elif oy in [3, 4, 5]: 
+    print("Bahor") 
+elif oy in [6, 7, 8]: 
+    print("Yoz") 
+else: 
+    print("Kuz")
